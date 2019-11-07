dc74287f4bc67bbf224e3adbfe178256493829ab
Boehm, Vanessa Martina (vboehm) <boehmvanessa@gmail.com>
2019-11-07 14:15:52 -0800

    renamed folder

--- pca_classifier/__init__.py
+++
@@ -1,1 +0,0 @@
-name='pca_classifier'
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='pca_classifier',
+setup(name='pca-classifier',
       use_scm_version=True,
       setup_requires=['setuptools_scm'],
       description='(regularized) linear data compression and its application to classification and out of distribution detection',

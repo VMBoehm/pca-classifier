578d1908b97a4b3720ac1c633b2dca3e4481e1e4
Boehm, Vanessa Martina (vboehm) <boehmvanessa@gmail.com>
2019-11-07 14:07:05 -0800

    adding package structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pca_classifier',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
+      description='(regularized) linear data compression and its application to classification and out of distribution detection',
+      url='http://github.com/VMBoehm/pca_classifier',
+      author='Vanessa Martina Boehm',
+      author_email='vboehm@berkeley.edu',
+      license='GNU General Public License v3.0',
+      packages=['pca_classifier'],
+      )
